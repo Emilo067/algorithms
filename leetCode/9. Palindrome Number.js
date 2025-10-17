c988e66fd17cb7d233fbe700d1771f23a2e83e82
Emilo067 <gasanovemil467@gmail.com>
2025-10-17 10:50:59 +0300

    9. Palindrome Number

---
+++ leetCode/9. Palindrome Number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = String(x)
+    let p1 = 0
+    let p2 = str.length - 1
+
+    while(p1 < p2) {
+        if(str[p1] !== str[p2]) {
+            return false
+        }
+        ++p1
+        --p2
+    }
+
+    return true
+};
